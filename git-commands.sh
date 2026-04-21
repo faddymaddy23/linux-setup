c31c201ef5c658784beb23fb6648e6ffd7c4635b
faddymaddy23 <faddymaddy23@gmail.com>
2026-04-21 20:48:48 +0000

    Added git commands script for version control

---
+++ git-commands.sh
@@ -0,0 +1,21 @@
+git clone <git_link>
+
+cd <folder_name>
+
+git checkout <branch_name>  
+
+git checkout -b <feature/feature_name>
+
+git add .
+
+git commit -m "added feature_name feature"   
+
+git push origin <feature/feature_name>
+
+git checkout <branch_name>   
+
+git merge --no-ff <feature/feature_name>
+git tag <1.0.1>
+git push origin <branch_name> --tags
+
+git pull origin <branch_name>
